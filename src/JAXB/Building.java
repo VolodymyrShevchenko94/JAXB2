61bfc12bdf87188cdb48536121dd39c6eef5a90c
volodymyrshevchenko <Chief_94@ukr.net>
2023-08-01 20:31:36 +0300

    done

---
+++ src/JAXB/Building.java
@@ -0,0 +1,20 @@
+package JAXB;
+
+public class Building {
+    private String addressBuilding;
+
+    public Building() {
+    }
+
+    public Building(String addressBuilding) {
+        this.addressBuilding = addressBuilding;
+    }
+
+    public String getAddressBuilding() {
+        return addressBuilding;
+    }
+
+    public void setAddressBuilding(String addressBuilding) {
+        this.addressBuilding = addressBuilding;
+    }
+}
---
+++ src/JAXB/CityCatalog.java
@@ -0,0 +1,27 @@
+package JAXB;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+
+
+@XmlRootElement(name = "City_catalog")
+public class CityCatalog {
+    private ArrayList<City> cities;
+
+    public CityCatalog() {
+    }
+
+    public CityCatalog(ArrayList<City> cities) {
+        this.cities = cities;
+    }
+
+    @XmlElement(name = "city")
+    public ArrayList<City> getCities() {
+        return cities;
+    }
+
+    public void setCities(ArrayList<City> cities) {
+        this.cities = cities;
+    }
+}
